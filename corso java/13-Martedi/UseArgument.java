0c7723ff38096203c53c574b2f6cc85d285dff91
Alexia8800 <136690305+Alexia8800@users.noreply.github.com>
2025-05-13 17:59:19 +0200

    Add files via upload

---
+++ corso java/13-Martedi/UseArgument.java
@@ -0,0 +1,14 @@
+public class UseArgument {
+ 
+        public static void main(String[] args) {
+       
+         String pippo = "pippo";
+       
+         System.out.print("Hi, "); 
+       
+         //grazie alla variabile che è pippo prenderà stampato quello che si sarà all'interno dell ""
+         System.out.print(pippo); 
+       
+         System.out.println(". How are you?"); 
+       
+       }}
